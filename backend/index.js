b25f22d83a59098588b7826be637fbb27e951b68
cascudo <lucas.sisau@gmail.com>
2020-01-14 22:44:28 -0300

    configure mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const env = require('.env')
+
 const app = express();
+mongoose.connect(env('DATABASE'), {
+    useNewUserParser: true,
+    useUnifiedTopology: true,
+});
+
 app.get('/', (request, response) => {
     return response.json({ message: 'olá mundo' });
 });
+
+app.post('/users', (request, response) => {
+
+});
+
 app.listen(80);
