c7b8f07e00c1ec2551b468ad1c0cff65d7bd495c
cascudo <lucas.sisau@gmail.com>
2020-01-18 13:44:45 -0300

    finish backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,16 +1,41 @@
 const axios = require('axios');
-const Dev = require('../models/Dev')
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
     async index(request, response) {
         const devs = await Dev.find();
         return response.json(devs);
     },
+    async show (request, response) {
+        const { _id } = request.params;
+        const dev = await Dev.findOne({ _id });
+        return response.json(dev);
+    },
+    async search(request, response) {
+        const { longitude, latitude, techs } = request.query;
+        const techsArray = parseStringAsArray(techs);
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
+        });
+        return response.json(devs);
+    },
     async store(request, response) {
-        const { github_username, techs, latitude, longitude } = request.body;
+        const { github_username, techs, longitude, latitude } = request.body;
         let dev = await Dev.findOne({ github_username });
         if (!dev) {
-            const techsArray = techs.split('.').map((tech) => tech.trim());
+            const techsArray = parseStringAsArray(techs);
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
             const { name = login, avatar_url, bio } = apiResponse.data;
             const location = {
@@ -27,5 +52,52 @@ module.exports = {
             });
         }
         return response.json(dev);
-    }
+    },
+    async update(request, response) {
+        const { _id } = request.params;
+        let dev = await Dev.findOne({ _id });
+        if (!dev) {
+            return response.json({
+                success: false,
+                message: 'Dev not found',
+            });
+        }
+        const { techs, longitude, latitude } = request.body;
+        const techsArray = parseStringAsArray(techs);
+        const apiResponse = await axios.get(`https://api.github.com/users/${dev.github_username}`);
+        const { name = login, avatar_url, bio } = apiResponse.data;
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+        };
+        dev.name = name;
+        dev.avatar_url = avatar_url;
+        dev.bio = bio;
+        dev.techs = techsArray;
+        dev.location = location;
+        dev.save();
+        response.json(dev);
+    },
+    async destroy(request, response) {
+        const { _id } = request.params;
+        const dev = await Dev.findOne({ _id });
+        if (!dev) {
+            return response.json({
+                success: false,
+                message: 'Dev not found',
+            });
+        }
+        dev.remove((error) => {
+            if (error) {
+                return response.json({
+                    success: false,
+                    message: error,
+                });
+            }
+            return response.json({
+                success: true,
+                message: 'Dev deleted successfully',
+            });
+        });
+    },
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,10 @@ routes.get('/', (request, response) => {
 });
 
 routes.get('/devs', DevController.index);
+routes.get('/devs/search', DevController.search);
+routes.get('/devs/:_id', DevController.show);
+routes.delete('/devs/:_id', DevController.destroy);
 routes.post('/devs', DevController.store);
+routes.put('/devs/:_id', DevController.update);
 
 module.exports = routes;
