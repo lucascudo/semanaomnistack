fae5c1a5ec3c109af9f3bf3dd572484dfa894717
cascudo <lucas.sisau@gmail.com>
2020-01-18 20:14:24 -0300

    Aula 03 - parte 1

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
+import './global.css';
 import './App.css';
+import './Sidebar.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="latitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+          <ul>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars2.githubusercontent.com/u/13350752" alt="Lucas Rocha" />
+                <div className="user-info">
+                  <strong>Lucas Rocha</strong>
+                  <span>NodeJS, ReactJS, ReactNative</span>
+                </div>
+              </header>
+              <p>Father, Developer and Tech Enthusiast.</p>
+              <a href="https://github.com/lucascudo">Acessar perfil no Github</a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars2.githubusercontent.com/u/13350752" alt="avatar" />
+                <div className="user-info">
+                  <strong>Lucas Rocha</strong>
+                  <span>NodeJS, ReactJS, ReactNative</span>
+                </div>
+              </header>
+              <p>Father, Developer and Tech Enthusiast.</p>
+              <a href="https://github.com/lucascudo">Acessar perfil no Github</a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars2.githubusercontent.com/u/13350752" alt="avatar" />
+                <div className="user-info">
+                  <strong>Lucas Rocha</strong>
+                  <span>NodeJS, ReactJS, ReactNative</span>
+                </div>
+              </header>
+              <p>Father, Developer and Tech Enthusiast.</p>
+              <a href="https://github.com/lucascudo">Acessar perfil no Github</a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars2.githubusercontent.com/u/13350752" alt="avatar" />
+                <div className="user-info">
+                  <strong>Lucas Rocha</strong>
+                  <span>NodeJS, ReactJS, ReactNative</span>
+                </div>
+              </header>
+              <p>Father, Developer and Tech Enthusiast.</p>
+              <a href="https://github.com/lucascudo">Acessar perfil no Github</a>
+            </li>
+          </ul>
+      </main>
     </div>
   );
 }
