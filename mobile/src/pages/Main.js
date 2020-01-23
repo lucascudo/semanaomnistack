15ff7c2ba50462df8d25bfd4dbbfc7d38f83cced
cascudo <lucas.sisau@gmail.com>
2020-01-23 19:24:59 -0300

    finish design

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons'
 
-function Main() {
+function Main({ navigation }) {
     const [ currentRegion, setCurrentRegion ] = useState(null);
     useEffect(() => {
         async function loadInitialPosition() {
@@ -29,18 +30,34 @@ function Main() {
     }
 
     return (
-        <MapView style={styles.map} initialRegion={currentRegion} >
-            <Marker coordinate={{ latitude: -12.9782988, longitude: -38.4616612 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/13350752?s=460&v=4' }} />
-                <Callout>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Lucas Rocha</Text>
-                        <Text style={styles.devBio}>Lorem ipsum dolor sit amet consectetur adipiscing elit</Text>
-                        <Text style={styles.devTechs}>ReactJS, React Native, NodeJS, Angular</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView style={styles.map} initialRegion={currentRegion} >
+                <Marker coordinate={{ latitude: -12.9782988, longitude: -38.4616612 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/13350752?s=460&v=4' }} />
+                    <Callout onPress={() => {
+                        navigation(navigate('Profile', { github_username: 'lucascudo' }))
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Lucas Rocha</Text>
+                            <Text style={styles.devBio}>Lorem ipsum dolor sit amet consectetur adipiscing elit</Text>
+                            <Text style={styles.devTechs}>ReactJS, React Native, NodeJS, Angular</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar dev por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrec={false}
+                />
+                <TouchableOpacity style={styles.loadButton} onPress={() => alert('pressed')}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -68,6 +85,39 @@ const styles = StyleSheet.create({
     },
     devTechs: {
         marginTop: 5,
+    },
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row', 
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '$834DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15, 
     },
 });
 
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -20,6 +20,7 @@ const Routes = createAppContainer(
         },
     }, {
         defaultNavigationOptions: {
+            headerBackTitleVisible: false,
             headerTintColor: '#FFF',
             headerStyle: {
                 backgroundColor: '#7D40E7',
