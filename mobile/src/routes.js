b1f3bd541a67c753780b512ee79fb5b41e31277a
cascudo <lucas.sisau@gmail.com>
2020-01-23 15:18:12 -0300

    correction

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,11 +1,11 @@
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigation } from 'react-navigation-stack';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Main from './pages/Main';
 import Profile from './pages/Profile';
 
 const Routes = createAppContainer(
-    createStackNavigation({
+    createStackNavigator({
         Main: {
             screen: Main,
             navigationOptions: {
