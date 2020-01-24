80afca1dd0421d0db764407feb9fdfe8f52960df
Lucas Rocha <lucas.rocha@cworks.com.br>
2020-01-24 15:03:35 -0300

    renames event

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -2,7 +2,7 @@ const socketio = require('socket.io');
 
 exports.setupWebsocket = (server) => {
     const io = socketio(server);
-    io.on('connection', (socket) => {
+    io.on('connect', (socket) => {
         console.log(socket.id);
         console.log(socket.handshake.query);
         socket.emit('message', 'Hello World');
